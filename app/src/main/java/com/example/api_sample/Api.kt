1d24bd1543bf9790980dc6ac82d00c6ee72ade3f
kamioka24 <keee2404@gmail.com>
2021-09-14 00:05:03 +0900

    [Fix]

--- app/src/main/java/com/example/api_sample/Api.kt
+++ app/src/main/java/com/example/api_sample/Api.kt
@@ -5,10 +5,10 @@ import retrofit2.http.GET
 
 interface Api {
     companion object {
-        const val BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lang=ja&q=wako&appid="
-        const val WEATHERINFO_URL = "81bee461624dcdbd3d18f881a1f2764d"
+        const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+        const val WEATHERINFO_URL = "weather?lang=ja&q=wako&appid=81bee461624dcdbd3d18f881a1f2764d"
     }
-    @GET("81bee461624dcdbd3d18f881a1f2764d")
+    @GET("weather?lang=ja&q=wako&appid=81bee461624dcdbd3d18f881a1f2764d")
     // 必ず suspend をつける
     suspend fun getApi(): Response<WeatherData>
 }
